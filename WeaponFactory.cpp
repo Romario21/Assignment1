5dd08278de5510c5f24268e9e1b774877ff829cc
Romario Estrella <rde16@my.fsu.edu>
2017-10-12 01:17:28 -0400

    modified WeaponFactory.cpp to include new classes and also modified getWeapon to have if statements that checked for these new weapons

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -9,6 +9,9 @@
 #include "WeaponFactory.h"
 #include "CommonSword.h" 
 #include "CommonSpear.h"
+#include "SimpleAxe.h"
+#include "CrazyRandomSword.h"
+#include "LegendaryTomahawk.h"
 
 WeaponFactory* WeaponFactory::instance = NULL;
 
@@ -26,6 +29,18 @@ Weapon * WeaponFactory::getWeapon(std::string name) {
     
     if (name.compare("spear") == 0) {
         return new CommonSpear();
+    }
+
+    if (name.compare("Tomahawk") == 0) {
+      return new LegendaryTomahawk();
+    }
+
+    if (name.compare("Crazy") == 0) {
+      return new CrazyRandomSword();
+    }
+
+    if (name.compare("Axe") == 0) {
+      return new SimpleAxe();
     }
 
     throw "Invalid weapon";
