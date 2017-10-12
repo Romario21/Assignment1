8122e797c63a3db151f29e5f148fcab8645fb6ef
Romario Estrella <rde16@my.fsu.edu>
2017-10-11 20:44:11 -0400

    Finished implimenting both LegendaryTomahawk files

--- LegendaryTomahawk.cpp
+++ LegendaryTomahawk.cpp
@@ -6,6 +6,23 @@
  *  Created on: Oct 11, 2017
  */
 
+#include "LegendaryTomahawk.h"
+
+
+double LegendaryTomahawk::hit(double armor){
+    double damage;
+
+    if(armor < 15)
+    	damage = hitPoints;
+
+    else
+    	damage = hitPoints - armor;
+
+    if(damage < 0)
+    	return 0;
+
+    return damage;
+}
 
 
 
--- LegendaryTomahawk.h
+++ LegendaryTomahawk.h
@@ -6,11 +6,28 @@
  *  Created on: Oct 11, 2017
  */
 
+#include <string>
+#include "Weapon.h"
+
 #ifndef LEGENDARYTOMAHAWK_H_
 #define LEGENDARYTOMAHAWK_H_
 
 
+/**
+ * Defines the behavior of a Legendary Tomahawk (hitpoint = 70, ignores armor points if less
+ * than 15)
+ */
 
+class LegendaryTomahawk : public Weapon {
+
+public:
+
+	SimpleAxe() : Weapon("Legendary Tomahawk", 70.0) {}
+	virtual ~LegendaryTomahawk() {};
+	virtual double hit(double armor);
+
+
+};
 
 
 #endif /* LEGENDARYTOMAHAWK_H_ */
