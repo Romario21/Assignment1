560e5eb6eafe3314b5680fd2e5cf5cbd99461ee5
Romario Estrella <rde16@my.fsu.edu>
2017-10-11 20:30:48 -0400

    Finished implimenting both SimpleAxe files

--- SimpleAxe.h
+++ SimpleAxe.h
@@ -7,12 +7,23 @@
  */
 
 #include <string>
+#include "Weapon.h"
 
 #ifndef SIMPLEAXE_H_
 #define SIMPLEAXE_H_
 
+/**
+ * Defines the behavior of a common spear (hitpoint = 60, ignores armor points if
+ * between 0 and 20)
+ */
+
 class SimpleAxe : public Weapon {
 
+public:
+
+	SimpleAxe() : Weapon("Simple Axe", 60.0) {}
+	virtual ~SimpleAxe() {};
+	virtual double hit(double armor);
 
 
 };
